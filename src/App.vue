02d567197274f6fb257a5e484462e223d28e83ab
b-oreilly <berniceoreilly@gmail.com>
2021-09-29 18:00:24 +0100

    card added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <ToDoList />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ToDoList from './components/ToDoList'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      ToDoList
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-card header="TodoList" header-tag="header">
+            <template v-slot:footer>
+                <b-button class="float-end" variant="primary">Add</b-button>
+            </template>
+        </b-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ToDoList',
+        components: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
