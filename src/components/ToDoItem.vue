d5ec4c15dd1156972cc0c5804cbabdbbb38fc65f
b-oreilly <berniceoreilly@gmail.com>
2021-10-04 10:55:49 +0100

    todo item added

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <b-list-group-item>
+            <span v-if="todo.done" class="done">{{ todo.text }}</span>
+            <span v-else>{{ todo.text }}</span>
+            <b-button class="float-end" pill variant="outline-danger" @click="deleteTodo(this.todo)">&#10005;
+            </b-button>
+            <span v-if="todo.done" class="float-end">&#128077;</span>
+            <b-button v-else class="float-end" pill variant="outline-success" @click="completeTodo(todo)">&#10003;
+            </b-button>
+        </b-list-group-item>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ToDoItem",
+        props: {
+            todo: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+    .list-group-item {
+        padding-block: 15px;
+    }
+
+</style>
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,18 +1,24 @@
 <template>
     <div>
         <b-card header="TodoList" header-tag="header">
+            <b-list-group>
+                <ToDoItem v-for="item in list" :key="item.id" :todo="item" />
+            </b-list-group>
             <template v-slot:footer>
-                <b-button class="float-end" variant="primary">Add</b-button>
+                <input type="text" v-model="todo" v-on:keyup.enter="addNewTodo()" placeholder="Add a Todo" />
+                <b-button class="float-end" variant="primary" @click="addNewTodo()">Add</b-button>
             </template>
         </b-card>
     </div>
 </template>
 
 <script>
+    import ToDoItem from '@/components/ToDoItem'
+
     export default {
         name: 'ToDoList',
         components: {
-
+            ToDoItem
         },
         data() {
             return {
@@ -31,7 +37,29 @@
                         text: "Create todo app using Vue",
                         done: false
                     }
-                ]
+                ],
+                todoText: ""
+            }
+        },
+        methods: {
+            addNewTodo() {
+                if (!this.todo) {
+                    alert("Please enter some text.");
+                    return;
+                }
+
+                // Getting the max ID in the list and adding 1 to it.
+                const newId = Math.max.apply(null, this.list.map(t => t.id)) + 1;
+                // Adding a todo to the list with a new id, the text entered in the input field and setting done to false.
+
+                this.list.push({
+                    id: newId,
+                    text: this.todo,
+                    done: false
+                });
+
+                // Reset todo to an empty string.
+                this.todo = "";
             }
         }
     }
